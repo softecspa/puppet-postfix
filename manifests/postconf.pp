e71bd59b80911cdb89a2399f835da78b7fd0aed8
root on puppetmaster <root@puppetmaster.vagrant.local>
2014-11-06 10:26:26 +0000

    fix check on unless with stripped string

--- manifests/postconf.pp
+++ manifests/postconf.pp
@@ -10,9 +10,12 @@ define postfix::postconf (
 
   $key_value = "${real_key}=${value}"
 
+  $stripped_value=regsubst($value,'\ ','')
+  $stripped_keyvalue = "${real_key}=${stripped_value}"
+
   exec{"postconf_${real_key}":
     command => "postconf -e '${key_value}'",
-    unless  => "test \"\$(postconf ${real_key} | sed -e 's/\\ //g')\" = '$key_value'",
+    unless  => "test \"\$(postconf ${real_key} | sed -e 's/\\ //g')\" = '$stripped_keyvalue'",
     path    => $::path
   }
 }
